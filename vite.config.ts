ac8aead51b0a69852d9d7bca0c58c5e49b32bbea
OpenRoad <lazaruseffect23@gmail.com>
2026-04-11 01:25:52 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/OpenRoadIO/',
   plugins: [react()],
+  base: '/OpenRoadIO/', // MUST match your repo name with slashes
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
